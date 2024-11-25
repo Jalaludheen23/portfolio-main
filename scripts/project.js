63f69669d40de67a175f2ebb970c7516defb2e85
Jalaludheen23 <155148382+Jalaludheen23@users.noreply.github.com>
2024-11-25 18:34:44 +0530

    Update project.js

--- scripts/project.js
+++ scripts/project.js
@@ -5,7 +5,7 @@ document.querySelector('[data-collapse-toggle]').addEventListener('click', funct
 });
 
 async function fetchProjects() {
-    const response = await fetch('https://jalaludheen23.github.io/portfolio-main/static/data/projects.json');
+    const response = await fetch('../static/data/projects.json');
     const projects = await response.json();
     return projects;
 }
